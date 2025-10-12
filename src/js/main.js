d0c6da82c24427b2532008d2cd6348e57329623c
Mehrab <mmehrab.pk@gmail.com>
2025-10-12 23:55:11 +0330

    pages added

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 // ----------------------navBar
 const navBtn = document.querySelectorAll('#navBar>ul>li')
 const navSpan = document.querySelector('#navBar>ul>span')
+const home = document.getElementById('home')
+const radio = document.getElementById('radio')
+const library = document.getElementById('library')
 
 let liNum = ''
 
@@ -21,6 +24,13 @@ navBtn.forEach((item, index, arr) => {
                 item.firstElementChild.setAttribute('fill', '#FA586A')
                 item.lastElementChild.classList.add('text-[#FA586A]')
                 item.lastElementChild.classList.remove('text-white')
+                radio.classList.remove('flex')
+                radio.classList.add('hidden')
+                library.classList.remove('flex')
+                library.classList.add('hidden')
+                home.classList.remove('hidden')
+                home.classList.add('flex')
+
 
                 break;
             case 1:
@@ -30,6 +40,12 @@ navBtn.forEach((item, index, arr) => {
                 item.firstElementChild.setAttribute('fill', '#FA586A')
                 item.lastElementChild.classList.add('text-[#FA586A]')
                 item.lastElementChild.classList.remove('text-white')
+                library.classList.remove('flex')
+                library.classList.add('hidden')
+                home.classList.remove('flex')
+                home.classList.add('hidden')
+                radio.classList.remove('hidden')
+                radio.classList.add('flex')
 
                 break;
             case 2:
@@ -39,6 +55,12 @@ navBtn.forEach((item, index, arr) => {
                 item.firstElementChild.setAttribute('fill', '#FA586A')
                 item.lastElementChild.classList.add('text-[#FA586A]')
                 item.lastElementChild.classList.remove('text-white')
+                radio.classList.remove('flex')
+                radio.classList.add('hidden')
+                home.classList.remove('flex')
+                home.classList.add('hidden')
+                library.classList.remove('hidden')
+                library.classList.add('flex')
 
                 break;
 
